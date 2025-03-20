263e519ceb378d71cc32aa1e6e42b25edb9d8dc8
jdacodes <jda.codes@gmail.com>
2025-03-20 18:48:25 +0800

    update navigation to use current screen from back stack entry and implement dashboard and favorites screens

--- app/src/main/java/com/jdacodes/graphqlanimedemo/AppNavigation.kt
+++ app/src/main/java/com/jdacodes/graphqlanimedemo/AppNavigation.kt
@@ -1,26 +1,44 @@
 package com.jdacodes.graphqlanimedemo
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun HomeNavigation() {
     val navController = rememberNavController()
+    val backStackEntry by navController.currentBackStackEntryAsState()
+    val currentScreen = backStackEntry?.destination?.let {
+        when (it.route) {
+            Navigation.Home.MediaListDetail.route -> Navigation.Home.MediaListDetail
+            Navigation.Home.Dashboard.route -> Navigation.Home.Dashboard
+            Navigation.Home.Favorites.route -> Navigation.Home.Favorites
 
+            else ->  Navigation.Home.MediaListDetail
+        }
+    }
     NavigationSuiteScaffold(
         navigationSuiteItems = {
             homeNavigationItems().forEach { (screen, icon) ->
+
                 item(
                     icon = { Icon(icon, contentDescription = screen.route) },
                     label = { Text(screen.route.replace("_", " ").replaceFirstChar { it.uppercase() }) },
                     selected = screen.route == navController.currentDestination?.route,
                     onClick = {
-                        navController.navigate(screen.route) {
+                        navController.navigate(screen) {
                             popUpTo(navController.graph.startDestinationId) { saveState = true }
                             launchSingleTop = true
                             restoreState = true
@@ -30,21 +48,40 @@ fun HomeNavigation() {
             }
         },
         content = {
-            NavHost(navController, startDestination = Navigation.Home.MediaListDetail) {
-                composable<Navigation.Home.Dashboard> { DashboardScreen() }
-                composable<Navigation.Home.MediaListDetail> { MediaListDetailPaneScaffold() }
-                composable<Navigation.Home.Favorites> { FavoritesScreen() }
-            }
+                NavHost(navController, startDestination = currentScreen ?: Navigation.Home.MediaListDetail) {
+                    composable<Navigation.Home.Dashboard> { DashboardScreen() }
+                    composable<Navigation.Home.MediaListDetail> { MediaListDetailPaneScaffold() }
+                    composable<Navigation.Home.Favorites> { FavoritesScreen() }
+                }
+
         }
     )
 }
 
 @Composable
 fun DashboardScreen() {
-    TODO("Not yet implemented")
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Text(
+            text = "Dashboard Screen",
+            modifier = Modifier.align(Alignment.Center)
+        )
+    }
 }
 
 @Composable
 fun FavoritesScreen() {
-    TODO("Not yet implemented")
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Text(
+            text = "Favorites Screen",
+            modifier = Modifier.align(Alignment.Center)
+        )
+    }
 }
